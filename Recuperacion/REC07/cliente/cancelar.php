a3953d768ab03c1516c80c109d77abfb9b8d54e7
alvaro <alvaro_martin_narganes02@hotmail.com>
2019-05-13 12:31:30 +0200

    sgsr

--- Recuperacion/REC07/cliente/cancelar.php
+++ Recuperacion/REC07/cliente/cancelar.php
@@ -18,7 +18,13 @@
     $sql="SELECT fecha_reserva FROM reservas where dni_cliente='$cliente'";
     $resultado=mysqli_query($link,$sql);
     $reg=mysqli_fetch_all($resultado,MYSQLI_NUM);
-    
+    $fechas="";
+    for ($i=0; $i <count($reg) ; $i++) { 
+        $fechas.="<option value='".$reg[$i][0]."'>".$reg[$i][0]."</option>";
+    }
+    echo"<form method='POST' action='reserva.php'> 
+    <label>Tus reservas: <select name='reserva'>".$fechas."</select></label><br>
+    </form>"
 
     ?>
 </body>
